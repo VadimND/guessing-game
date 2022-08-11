c966232c7a70c4f453088333b06697e7c6622763
VadimND <webzorka24@gmail.com>
2022-08-11 15:31:23 +0300

    feat: add class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max, result) {
+        this.min = min;
+        this.max = max;
+        this.result = result;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.max - this.min) / 2) + this.min;
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
